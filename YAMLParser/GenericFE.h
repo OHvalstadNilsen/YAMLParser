c513dca2c100fbf115b6e8e4b78f20d0aca902a1
OHvalstadNilsen <oskar.h.nilsen@gmail.com>
2019-02-28 13:47:00 +0100

    Added interface for FEM elements

---
+++ YAMLParser/GenericFE.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "yaml-cpp/yaml.h"
+
+class GenericFE {
+	/*Interface defining the minimum content of a 
+	* FEM element object.
+	*
+	* All FEM element objects are based on this interface
+	*/
+public:
+	//Console print for debugging:
+	virtual void printAttributes() = 0;
+	
+protected:
+	//Setters for attrubute values:
+	virtual bool setMandatoryValues(YAML::Node& yamlNode) = 0;
+	virtual void setOptionalValues(YAML::Node& yamlNode) = 0;
+	
+	//External userdefined element ID:
+	int id;
+};
