4e3586ebf0c19552b873f9fc50ed05a4b7772cd0
OHvalstadNilsen <oskar.h.nilsen@gmail.com>
2019-03-05 15:07:47 +0100

    Added description

--- YAMLParser/FETypes.h
+++ YAMLParser/FETypes.h
@@ -1,4 +1,11 @@
 #pragma once
+/*This is a collective header file for all
+* FEM element types defined in the parser.
+* 
+* Including this file in other .h or .cpp 
+* files will include all the defined FEM
+* element types.
+*/
 
 #include "FENode.h"
 #include "FEBeam.h"
