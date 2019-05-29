194cd60f84cdec7e679bf3b1fa158d4a05dcc734
OHvalstadNilsen <oskar.h.nilsen@gmail.com>
2019-05-29 11:44:19 +0200

    Removed deprecated constructor

--- YAMLParser/FENode.h
+++ YAMLParser/FENode.h
@@ -18,7 +18,7 @@ class FENode :
 	public GenericFE
 {
 public:
-	FENode(int id, double x, double y, double z, int ix = 0, int iy = 0, int iz = 0, int irx = 0, int iry = 0, int irz = 0, int rotID = 0);
+	//FENode(int id, double x, double y, double z, int ix = 0, int iy = 0, int iz = 0, int irx = 0, int iry = 0, int irz = 0, int rotID = 0);
 	FENode(YAML::Node& yamlNode, FECoordSys* rot);
 	~FENode();
 
